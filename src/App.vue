0aaa4d1d4bc149a8a13c5e9155e72cf38fafb8bc
Arthur Parienté <arthur.pariente78960@gmail.com>
2025-09-05 17:50:26 +0200

    better shadcn support

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,28 @@
-
-
 <template>
-	<main class="min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-50 to-gray-200 dark:bg-gradient-to-br dark:from-gray-900 dark:to-gray-800">
-		<section class="w-full max-w-lg bg-white dark:bg-gray-900 rounded-xl shadow-lg p-8 space-y-8">
-			<h1 class="text-3xl font-bold text-center text-gray-900 dark:text-gray-100 mb-6">Aviso Installer</h1>
-	  <p class="text-gray-600 dark:text-gray-300 text-sm text-justify">
-		This tool will copy the content of each section from your AVISO file into your LFXX file. You need to make sure that the sections are correctly defined in the AVISO file. Please note that this tool will simply append the content of the AVISO to the LFXX file. There is no logic for merging or updating, so you cannot update an installed AVISO. Instead, you must use a blank original LFXX file (without the AVISO) and then install it.
-	  </p>
-			<FilePicker />
-		</section>
-	</main>
-
-	<Toaster />
+	<div class="flex min-h-screen items-center justify-center bg-background">
+		<Card class="w-full max-w-lg">
+			<CardHeader>
+				<CardTitle class="text-center text-3xl">Aviso Installer</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<p class="text-muted-foreground text-justify mb-4 text-sm">
+					This tool will copy the content of each section from your AVISO file into your LFXX file. You need
+					to make sure that the sections are correctly defined in the AVISO file. Please note that this tool
+					will simply append the content of the AVISO to the LFXX file. There is no logic for merging or
+					updating, so you cannot update an installed AVISO. Instead, you must use a blank original LFXX file
+					(without the AVISO) and then install it.
+				</p>
+				<FilePicker />
+			</CardContent>
+		</Card>
+		<Toaster />
+	</div>
 </template>
 
+
 <script setup lang="ts">
-import FilePicker from './components/FilePicker.vue'
+import FilePicker from '@/components/FilePicker.vue'
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Toaster } from '@/components/ui/sonner'
-import 'vue-sonner/style.css' // vue-sonner v2 requires this import
+import 'vue-sonner/style.css'
 </script>
